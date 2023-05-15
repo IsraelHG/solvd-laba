faa1458c2626d201096bbcc336d9486a0698ea8c
IsraelHG <israael0042@gmail.com>
2023-05-15 01:05:22 -0700

    syntax fixed for LOGGER

--- src/main/java/com/solvd/laba/lab2/Manager.java
+++ src/main/java/com/solvd/laba/lab2/Manager.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.ArrayList;
 
 public class Manager extends Employee implements Manageable {
-    public static final Logger logger = LogManager.getLogger(Manager.class.getName());
+    public static final Logger LOGGER = LogManager.getLogger(Manager.class.getName());
 
     protected ArrayList<Employee> employees;
 
@@ -25,16 +25,16 @@ public class Manager extends Employee implements Manageable {
 
     @Override
     public void browse() {
-        logger.info(this.name + " is browsing the store...");
+        LOGGER.info(this.name + " is browsing the store...");
     }
 
     @Override
     public void restock() {
-        logger.info(this.name + " is restocking shelves...");
+        LOGGER.info(this.name + " is restocking shelves...");
     }
 
     @Override
     public void manage() {
-        logger.info(this.name + " is managing employees...");
+        LOGGER.info(this.name + " is managing employees...");
     }
 }
